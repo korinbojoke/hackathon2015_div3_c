2bfa6d4da16e478e4216553de91e171bdd83d4ea
shimpei <1st.link.sukimax@gmail.com>
2015-08-28 23:13:55 +0900

    gazo ok

--- app/controllers/ohakas_controller.rb
+++ app/controllers/ohakas_controller.rb
@@ -1,5 +1,6 @@
 class OhakasController < ApplicationController
   def index
+    @ohakas = Ohaka.all
   end
 
   def new
@@ -11,12 +12,32 @@ class OhakasController < ApplicationController
     redirect_to @ohaka
   end
 
+  def edit
+    @ohaka = Ohaka.find(params[:id])
+  end
+
   def show
     @ohaka = Ohaka.find(params[:id])
   end
 
+  def update
+    @ohaka = Ohaka.find(params[:id])
+
+    if @ohaka.update(ohaka_params)
+      redirect_to @ohaka
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @ohaka = Ohaka.find(params[:id])
+    @ohaka.destroy
+    redirect_to ohakas_path
+  end
+
   private
   def ohaka_params
-    params.require(:ohaka).permit(:name,:image,:location,:activity,:orin_num,:senko_num)
+    params.require(:ohaka).permit(:name,:image,:image_cache, :remove_image,:location,:activity,:orin_num,:senko_num)
   end
 end
--- app/models/ohaka.rb
+++ app/models/ohaka.rb
@@ -1,4 +1,5 @@
 class Ohaka < ActiveRecord::Base
+  validates :name, presence: true
   # 画像の追加
   mount_uploader :image, ImageUploader
 end
